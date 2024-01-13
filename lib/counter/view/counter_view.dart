81e421bf1f408265f755e3eba1ff49548e065e14
Bega007 <begajan007@gmail.com>
2024-01-13 08:00:41 +0500

    v 1.0.0

---
+++ lib/counter/view/counter_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_counter/counter/cubit/counter_cubit.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(builder: (context, state) {
+          return Text(
+            '$state',
+            style: textTheme.displayMedium,
+          );
+        }),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            key: const Key('increment'),
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          FloatingActionButton(
+            key: const Key(''),
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_counter/app.dart';
+import 'package:flutter_counter/counter_observer.dart';
+
+void main() {
+  Bloc.observer = const CounterObserver();
+  runApp(const CounterApp());
+}
